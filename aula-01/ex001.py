f411d69ecffc339a94ee5e238193369137b0186a
senaisolucoesti <senai.solucoesti@gmail.com>
2026-06-19 11:10:29 -0300

    Update ex001.py

--- aula-01/ex001.py
+++ aula-01/ex001.py
@@ -1,2 +1,4 @@
-print(7 + 4) # resultado: 11
-print("7" + "4") # resultado: 74
+nome = input("Qual é o seu nome? ")
+idade = input("Quantos anos você tem? ")
+peso = input("Quanto você pesa? ")
+print(f"Olá {nome}, você tem {idade} anos e pesa {peso} kg.")
